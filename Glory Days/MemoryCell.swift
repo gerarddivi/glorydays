81edfb7f51c3c6fbf508d885bf5aba4b38a7be66
Gerard Divi <gerarditus@gmail.com>
2016-10-02 16:44:20 +0200

    Revert "Revert "Initial Commit""
    This reverts commit 85fa9d43850a3feab14a27b6bfc68ab3a3215438.

---
+++ Glory Days/MemoryCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemoryCell.swift
+//  Glory Days
+//
+//  Created by Gerard Divi on 2/10/16.
+//  Copyright © 2016 Gerard Divi. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+}
